33f87e51e9f1bf532f953b6ff915cb565ec6a777
Sahil-husen <sahilhussainsahil76@gmail.com>
2024-08-02 11:54:38 +0530

    Use State Basics

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Card2 from './components/card2'
  import Card from './components/card'
 
 function App() {
   
   return(
     <div>
-      <Card/>
-      
+      {/* <Card/> */}
+      <Card2/>
     </div>
   )
  
---
+++ first-app/src/components/card2.jsx
@@ -0,0 +1,23 @@
+import '../components/card2.css'
+function card2(){
+    return(
+     <div>
+        <div className='container'>
+            <div className="song">
+                 <h1>Mahiya ve</h1>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolores iste cupiditate.</p>
+                <button>Download</button>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className="song">
+                 <h1>Mahiya ve</h1>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolores iste cupiditate.</p>
+                <button>Download</button>
+            </div>
+        </div>
+     </div>
+    )
+}
+export default card2;
